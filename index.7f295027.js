a964a9d2256b1938889862f1cc30677736411ae0
CherniukAnna <116806930+CherniukAnna@users.noreply.github.com>
2023-06-13 12:30:30 +0000

    Deploying to gh-pages from @ CherniukAnna/Project-fresh@f8c0abc6b2e834b9cfb9c346bab933771e91f29a 🚀

--- index.7f295027.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#burger"),c=document.querySelector(".nav__block");e.addEventListener("click",(()=>{c.classList.toggle("open")}));
-//# sourceMappingURL=index.7f295027.js.map
--- index.e27675c0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#burger"),c=document.querySelector(".nav__block");e.addEventListener("click",(function(){c.classList.toggle("open")}))}();
-//# sourceMappingURL=index.e27675c0.js.map
